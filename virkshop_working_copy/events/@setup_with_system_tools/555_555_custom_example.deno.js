d82721decc4d910edc4f1cd0f26ca46021fa7b0d
Jeff Hykin <jeff.hykin@gmail.com>
2025-01-23 19:05:16 -0600

    help with refactoring

---
+++ virkshop_working_copy/events/@setup_with_system_tools/555_555_custom_example.deno.js
@@ -0,0 +1,6 @@
+
+// // uncomment to show the zsh version
+// import { FileSystem } from "https://deno.land/x/quickr@0.6.66/main/file_system.js"
+// import { Console } from "https://deno.land/x/quickr@0.6.66/main/console.js"
+// import $ from "https://deno.land/x/dax@0.39.2/mod.ts"
+// console.log(await $`zsh --version`.text())
