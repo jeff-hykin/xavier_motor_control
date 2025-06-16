24c8055904139a92eeab4bf25e3c49437eb46f5f
Jeff Hykin <jeff.hykin@gmail.com>
2025-06-16 15:41:08 -0500

    get scons working

--- virkshop/events/@setup_with_system_tools/555_555_custom_example.zsh
+++ virkshop/events/@setup_with_system_tools/555_555_custom_example.zsh
@@ -2,4 +2,8 @@
 # export PATH="/usr/bin:$PATH"
 
 # # uncomment for a small command line shortcut
-# alias ll="ls -l"
+alias ll="ls -l"
+touch "$HOME/.zsh_history"
+
+# Setup scons (otherwise it will be broken) this is workaround for a nix problem
+export SCONS_LIB_DIR="$(dirname "$(dirname "$(which scons)")")/lib/python3.11/site-packages/scons"
