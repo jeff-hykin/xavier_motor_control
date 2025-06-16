5d6d310a77989dd6077be663a865452110b0cb58
Jeff Hykin <jeff.hykin@gmail.com>
2025-06-16 13:51:40 -0500

    -

--- main/python/cycle_motors.py
+++ main/python/cycle_motors.py
@@ -1,38 +1,60 @@
 import Jetson.GPIO as GPIO
 import time
 
-PWM_PIN = 33
-FREQ = 50 # Hz
+# 
+# setup
+# 
+if True:
+    PWM_PIN = 33
+    FREQ = 50 # Hz
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PWM_PIN, GPIO.OUT)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(PWM_PIN, GPIO.OUT)
 
-pwm = GPIO.PWM(PWM_PIN, FREQ)
+    pwm = GPIO.PWM(PWM_PIN, FREQ)
 
-def set_pulse_width(duty_cycle_microseconds_delay):
-    print(f'''angle = {angle}''')
-    percent = (duty_cycle_microseconds_delay * FREQ) / 10000
-    pwm.ChangeDutyCycle(percent)
+    def set_pulse_width(duty_cycle_microseconds_delay):
+        print(f'''angle = {angle}''')
+        percent = (duty_cycle_microseconds_delay * FREQ) / 10000
+        pwm.ChangeDutyCycle(percent)
 
 
-xavier_min_angle = 1000 # microseconds pulse width
-xavier_max_angle = 2000 # microseconds pulse width
-def angle_to_pulse_with(angle):
-    # (0 to 360) to (1000 to 2000)
-    return ((angle % 360/360)*1000)+1000
+    xavier_min_angle = 1000 # microseconds pulse width
+    xavier_max_angle = 2000 # microseconds pulse width
+    def angle_to_pulse_with(angle):
+        # (0 to 360) to (1000 to 2000)
+        return ((angle % 360/360)*1000)+1000
 
-try:
-    pwm.start(0)
+
+
+# 
+# helpers
+# 
+def test_range():
     angle = 0
-    change_amount = 10
+    degrees_of_change = 10
     while True:
-        if angle + change_amount > 360 or angle + change_amount < 0:
-            change_amount = -change_amount
+        if angle + degrees_of_change > 360 or angle + degrees_of_change < 0:
+            degrees_of_change = -degrees_of_change
         
-        angle += change_amount
+        angle += degrees_of_change
         print(f'''angle = {angle}''')
         set_pulse_width(angle_to_pulse_with(angle))
         time.sleep(1)
+try:
+    pwm.start(0)
+    while 1:
+        angle = 110 # start 
+        degrees_of_change = 10
+        wait_time = 0.2 # sec
+        while True:
+            if angle + degrees_of_change > 200 or angle + degrees_of_change < 109:
+                degrees_of_change = -degrees_of_change
+            
+            angle += degrees_of_change
+            print(f'''angle = {angle}''')
+            set_pulse_width(angle_to_pulse_with(angle))
+            time.sleep(wait_time)
 finally:
     pwm.stop()
     GPIO.cleanup()
